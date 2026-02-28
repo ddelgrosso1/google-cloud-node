b42cfbe2b2e8327e3e800325ed3fdf18d730f2af
Gautam Sharda <gautamsharda@google.com>
2026-02-28 00:50:27 +0000

    chore: add required argument

--- handwritten/error-reporting/owlbot.py
+++ handwritten/error-reporting/owlbot.py
@@ -21,7 +21,7 @@ import os
 logging.basicConfig(level=logging.DEBUG)
 
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_mono_repo_library()
+templates = common_templates.node_mono_repo_library(relative_dir="handwritten/error-reporting")
 s.copy(templates, destination="handwritten/error-reporting")
 node.fix_hermetic(relative_dir="handwritten/error-reporting")
 
